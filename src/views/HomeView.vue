90ade0830e451f8d3f195081270e090ac51cff99
Marcus Reis <mvsantosreis@gmail.com>
2024-05-26 20:28:32 +0100

    Clean installation + Hello World

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { Button } from '@/components/ui/button'
 </script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="p-5">Hello World</h1>
+    <Button>Click me</Button>
   </main>
 </template>
