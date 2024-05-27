a09fbf7eb01eb7aef3f1a7aea6a15cfd41b4c211
Marcus Reis <mvsantosreis@gmail.com>
2024-05-27 18:23:07 +0100

    Product Modal Call

---
+++ src/components/ProductModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <Modal :isOpen="isOpen" @on-close="onClose">
+    <div>Product Modal</div>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from '@/components/ui/Modal.vue'
+
+import productModal from '@/composables/useProductModal'
+const { isOpen, onClose } = productModal()
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import ProductModal from '@/components/ProductModal.vue'
+
+import productModal from '@/composables/useProductModal'
+const { onOpen, isOpen } = productModal()
 </script>
 <template>
-  <main>
-    <h1 class="p-5">Hello World</h1>
-    <Button>Click me</Button>
-    <Button variant="destructive">Click me</Button>
-  </main>
+  <ProductModal v-if="isOpen" />
+  <div class="mx-auto w-full max-w-4xl my-10">
+    <div class="flex justify-between items-center">
+      <h3 class="text-2xl font-bold">All Products</h3>
+      <Button @click="onOpen">Add Product</Button>
+    </div>
+  </div>
 </template>
