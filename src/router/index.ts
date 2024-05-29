b35b101542c7510b054a2d48e002949c7bbe31fb
Marcus Reis <mvsantosreis@gmail.com>
2024-05-29 12:30:09 +0100

    User token and route management

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import Login from '@/views/Auth/Login.vue'
 import Register from '@/views/Auth/Register.vue'
-import Category from '../views/Category.vue'
-import SingleProduct from '../views/products/[id].vue'
+import Category from '@/views/Category.vue'
+import SingleProduct from '@/views/products/[id].vue'
+import { isUserLoggedIn } from '@/lib/utils'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,5 +45,19 @@ const router = createRouter({
     }
   ]
 })
+router.beforeEach(async (to, from, next) => {
+  const isLoggedIn = await isUserLoggedIn()
+  // const isLoggedIn = true;
 
+  if (to.name !== 'auth-login' && !isLoggedIn) {
+    if (!to.meta.auth) {
+      return next()
+    }
+    return next({ name: 'auth-login' })
+  } else if (to.name === 'auth-login' && isLoggedIn) {
+    return next({ name: 'home' })
+  } else {
+    next()
+  }
+})
 export default router
